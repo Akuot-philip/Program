648d1da406a7955a001ff066b5168cb68c83d6b0
Akuot-philip <akuotphilipp@gmail.com>
2022-04-18 14:08:54 +0300

    program that prints text

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main(){
+println("Cliff Gor is My mentor")
+    study()
+}
+fun study(){
+    var name="Akuot Philip"
+    var school="Akirachix"
+    var career=("Software engineer")
+    var mentor=("CLiff Gor")
+    var information=("My name is $name i am currently a student at $school ,my career is to be $career and my mentor is $mentor he is a $career")
+    println(information)
+
+}
